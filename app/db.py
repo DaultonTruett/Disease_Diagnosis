156d342da94044802770a845a5343f3142d32b59
Daulton Truett <dtruett45@gmail.com>
2025-08-03 08:42:58 -0600

    Added query methods along with a helper method to convert the results of queries into a pandas dataframe.

--- app/db.py
+++ app/db.py
@@ -53,3 +53,75 @@ class DB:
                 
         except(ValueError) as e:
             print(e)
+
+
+    def queryAll(self):
+        cursor = self.conn.cursor()
+        
+        query = """SELECT * FROM disease_diagnosis;""";
+        cursor.execute(query);
+        
+        df = self.convertToDataframe(cursor.fetchall(), cursor.description);
+        
+        cursor.close();
+        
+        return df;
+    
+    
+    def querySymptoms(self):
+        cursor = self.conn.cursor();
+        
+        query = """SELECT symptom_1, symptom_2, symptom_3,
+            count(symptom_1) as symptom_1_count,
+            count(symptom_2) as symptom_2_count,
+            count(symptom_3) as symptom_3_count
+            FROM disease_diagnosis
+            GROUP BY symptom_1, symptom_2, symptom_3
+            ORDER BY symptom_1, symptom_2, symptom_3 DESC
+            LIMIT 10;""";
+
+        cursor.execute(query);
+        
+        df = self.convertToDataframe(cursor.fetchall(), cursor.description);
+        
+        cursor.close();
+        
+        return df;
+
+
+    def queryDiagnosisCount(self):
+        cursor = self.conn.cursor();
+        
+        query = """SELECT diagnosis,
+            count(diagnosis) as diagnosis_count
+            FROM disease_diagnosis
+            GROUP BY diagnosis;""";
+
+        cursor.execute(query);
+        
+        df = self.convertToDataframe(cursor.fetchall(), cursor.description);
+        
+        cursor.close();
+        
+        return df;
+    
+    
+    def queryAgeCount(self):
+        cursor = self.conn.cursor();
+        
+        query = """SELECT age, count(age) 
+            FROM disease_diagnosis
+            GROUP BY age
+            ORDER BY age;""";
+            
+        cursor.execute(query);
+        
+        df = self.convertToDataframe(cursor.fetchall(), cursor.description);
+        
+        cursor.close();
+        
+        return df;
+
+
+    def convertToDataframe(self, data, description):
+        return pd.DataFrame.from_records(data, columns=[name[0] for name in description]);
